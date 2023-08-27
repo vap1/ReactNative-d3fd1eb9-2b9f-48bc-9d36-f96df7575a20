bcf9e35c3a9de7df069c53ae9a179d00628fcc5e
vap1 <vaibhavvasant1705@gmail.com>
2023-08-27 16:17:33 +0530

    First Commit

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,16 +1,20 @@
 
 import React, { useState } from 'react';
-import { View, TextInput, Button, FlatList, Text } from 'react-native';
-import { User } from '../types/UserTypes';
+import { View, Text, TextInput, Button } from 'react-native';
+import { SearchRequest, SearchResponse } from '../types/SearchTypes';
 import { search } from '../apis/SearchApi';
 
 const SearchScreen: React.FC = () => {
   const [keyword, setKeyword] = useState('');
-  const [searchResults, setSearchResults] = useState<User[]>([]);
+  const [searchResults, setSearchResults] = useState<SearchResponse[]>([]);
 
   const handleSearch = async () => {
     try {
-      const response = await search(keyword);
+      const request: SearchRequest = {
+        keyword: keyword,
+      };
+
+      const response = await search(request);
       setSearchResults(response.searchResults);
     } catch (error) {
       console.error('Error searching:', error);
@@ -19,24 +23,21 @@ const SearchScreen: React.FC = () => {
 
   return (
     <View>
+      <Text>Search Screen</Text>
       <TextInput
         placeholder="Enter keyword"
         value={keyword}
         onChangeText={setKeyword}
       />
       <Button title="Search" onPress={handleSearch} />
-      <FlatList
-        data={searchResults}
-        keyExtractor={(item) => item.userId}
-        renderItem={({ item }) => (
-          <View>
-            <Text>{item.firstName} {item.lastName}</Text>
-            <Text>Email: {item.email}</Text>
-            <Text>Phone: {item.phone}</Text>
-            <Text>Address: {item.address}</Text>
-          </View>
-        )}
-      />
+      {searchResults.map((result) => (
+        <View key={result.userId}>
+          <Text>{result.firstName} {result.lastName}</Text>
+          <Text>Email: {result.email}</Text>
+          <Text>Phone: {result.phone}</Text>
+          <Text>Address: {result.address}</Text>
+        </View>
+      ))}
     </View>
   );
 };
