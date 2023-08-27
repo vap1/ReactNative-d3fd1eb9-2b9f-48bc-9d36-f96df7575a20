0d738f37a94f2fcb002fda588874e3b3a5cc7b75
vap1 <vaibhavvasant1705@gmail.com>
2023-08-27 16:17:38 +0530

    First Commit

--- src/apis/DataDisplayApi.ts
+++ src/apis/DataDisplayApi.ts
@@ -2,14 +2,17 @@
 import axios from 'axios';
 import { User } from '../types/UserTypes';
 
-const BASE_URL = 'http://your-api-base-url.com'; // Replace with your actual API base URL
+export interface DataDisplayRequest {}
 
-export const getDataDisplay = async (): Promise<User[]> => {
+export interface DataDisplayResponse {
+  users: User[];
+}
+
+export const getDataDisplay = async (): Promise<DataDisplayResponse> => {
   try {
-    const response = await axios.get<User[]>(`${BASE_URL}/data-display`);
+    const response = await axios.get<DataDisplayResponse>('/data-display');
     return response.data;
   } catch (error) {
-    console.error('Error fetching data display:', error);
-    throw error;
+    throw new Error('Failed to fetch data display');
   }
 };
