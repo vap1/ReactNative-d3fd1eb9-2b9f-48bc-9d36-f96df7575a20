6b32044ab7b7ab2ba1dc1174f1ad05ded3e5d588
vap1 <vaibhavvasant1705@gmail.com>
2023-08-27 16:17:37 +0530

    First Commit

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-
 import DataInputScreen from '../screens/DataInputScreen';
 import SearchScreen from '../screens/SearchScreen';
 
