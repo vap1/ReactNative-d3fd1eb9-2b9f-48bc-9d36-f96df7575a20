d0086fcf80364a59811c271f098ff17ff5406caf
vap1 <vaibhavvasant1705@gmail.com>
2023-08-27 16:17:33 +0530

    First Commit

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -2,58 +2,30 @@
 import React from 'react';
 import { View, Text, FlatList } from 'react-native';
 import { User } from '../types/UserTypes';
-import { search } from '../apis/SearchApi';
+import { SearchResponse } from '../apis/SearchApi';
 
 type SearchResultsProps = {
-  keyword: string;
-};
-
-type SearchResultsState = {
   searchResults: User[];
 };
 
-class SearchResults extends React.Component<SearchResultsProps, SearchResultsState> {
-  constructor(props: SearchResultsProps) {
-    super(props);
-    this.state = {
-      searchResults: [],
-    };
-  }
-
-  componentDidMount() {
-    this.fetchSearchResults();
-  }
-
-  fetchSearchResults = async () => {
-    try {
-      const response = await search(this.props.keyword);
-      this.setState({ searchResults: response.searchResults });
-    } catch (error) {
-      console.error('Error fetching search results:', error);
-    }
-  };
-
-  renderSearchResult = ({ item }: { item: User }) => (
+const SearchResults: React.FC<SearchResultsProps> = ({ searchResults }) => {
+  return (
     <View>
-      <Text>{item.firstName} {item.lastName}</Text>
-      <Text>Email: {item.email}</Text>
-      <Text>Phone: {item.phone}</Text>
-      <Text>Address: {item.address}</Text>
+      <Text>Search Results:</Text>
+      <FlatList
+        data={searchResults}
+        keyExtractor={(item) => item.userId}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.firstName} {item.lastName}</Text>
+            <Text>Email: {item.email}</Text>
+            <Text>Phone: {item.phone}</Text>
+            <Text>Address: {item.address}</Text>
+          </View>
+        )}
+      />
     </View>
   );
-
-  render() {
-    return (
-      <View>
-        <Text>Search Results for: {this.props.keyword}</Text>
-        <FlatList
-          data={this.state.searchResults}
-          renderItem={this.renderSearchResult}
-          keyExtractor={(item) => item.userId}
-        />
-      </View>
-    );
-  }
-}
+};
 
 export default SearchResults;
